adc40df8aba01de5e90c09209e2a5ee3b6f1aaa8
cathyuwu <cathywu312@gmail.com>
2024-09-18 11:22:39 -0400

    personalized website

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -36,13 +36,13 @@ const CreatePage = () => {
 		<Container maxW={"container.sm"}>
 			<VStack spacing={8}>
 				<Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
-					Create New Product
+					Add New Wish
 				</Heading>
 
 				<Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
 					<VStack spacing={4}>
 						<Input
-							placeholder='Product Name'
+							placeholder='Item Name'
 							name='name'
 							value={newProduct.name}
 							onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
@@ -62,7 +62,7 @@ const CreatePage = () => {
 						/>
 
 						<Button colorScheme='blue' onClick={handleAddProduct} w='full'>
-							Add Product
+							Add Wish!
 						</Button>
 					</VStack>
 				</Box>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
 					bgClip={"text"}
 					textAlign={"center"}
 				>
-					Current Products 🚀
+					Current Wants 🛍️✨
 				</Text>
 
 				<SimpleGrid
@@ -41,10 +41,10 @@ const HomePage = () => {
 
 				{products.length === 0 && (
 					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-						No products found 😢{" "}
+						No wishes yet 😓{" "}
 						<Link to={"/create"}>
 							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-								Create a product
+								Add a new wish 🌟
 							</Text>
 						</Link>
 					</Text>
